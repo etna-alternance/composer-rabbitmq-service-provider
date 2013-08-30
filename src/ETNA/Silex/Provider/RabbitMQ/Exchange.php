baf55c2238adefc2d398056d117e0b84c52d90bc
Stephane CRIVISIER <stephane@crivisier.fr>
2013-08-30 17:35:41 +0200

    Fix undefined $options

--- src/ETNA/Silex/Provider/RabbitMQ/Exchange.php
+++ src/ETNA/Silex/Provider/RabbitMQ/Exchange.php
@@ -5,7 +5,7 @@ namespace ETNA\Silex\Provider\RabbitMQ;
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Channel\AMQPChannel;
 /**
- * 
+ *
  */
 class Exchange
 {
@@ -20,45 +20,47 @@ class Exchange
     {
         $this->name        = $name;
         $this->channel     = $channel;
-        $this->type        = $options["type"];
-        $this->passive     = $options["passive"];
-        $this->durable     = $options["durable"];
-        $this->auto_delete = $options["auto_delete"];
+        $this->type        = isset($options["type"])        ? $options["type"]        : "direct";
+        $this->passive     = isset($options["passive"])     ? $options["passive"]     : false;
+        $this->durable     = isset($options["durable"])     ? $options["durable"]     : true;
+        $this->auto_delete = isset($options["auto_delete"]) ? $options["auto_delete"] : false;
 
-        $channel->exchange_declare(
-            $name,
-            $this->type,
-            $this->passive,
-            $this->durable,
-            $this->auto_delete
-        );
+        if ($name != "") {
+            $channel->exchange_declare(
+                $name,
+                $this->type,
+                $this->passive,
+                $this->durable,
+                $this->auto_delete
+            );
+        }
     }
 
     public function getName()
     {
         return $this->name;
     }
-    
+
     public function getChannel()
     {
         return $this->channel;
     }
-    
+
     public function getType()
     {
         return $this->type;
     }
-    
+
     public function isPassive()
     {
         return $this->passive;
     }
-    
+
     public function isDurable()
     {
         return $this->durable;
     }
-    
+
     public function isAutoDelete()
     {
         return $this->auto_delete;
